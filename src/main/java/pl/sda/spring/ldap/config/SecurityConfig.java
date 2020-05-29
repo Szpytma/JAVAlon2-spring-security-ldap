7743753e14cbf36652fe28a1ca76c79f5216bb6f
kochanskicezary <c.kochanski93+1@gmail.com>
2020-05-30 00:07:23 +0200

    Add sample spring security ldap app

---
+++ src/main/java/pl/sda/spring/ldap/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package pl.sda.spring.ldap.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/developers").hasRole("DEVELOPERS")
+            .antMatchers("/managers").hasRole("MANAGERS")
+            .antMatchers("/**").fullyAuthenticated()
+            .and()
+            .formLogin();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .ldapAuthentication()
+            .userDnPatterns("uid={0},ou=people")
+            .groupSearchBase("ou=groups")
+            .contextSource()
+            .url("ldap://localhost:8389/dc=springframework,dc=org")
+            .and()
+            .passwordCompare()
+            .passwordEncoder(new BCryptPasswordEncoder())
+            .passwordAttribute("userPassword");
+    }
+}
---
+++ src/main/java/pl/sda/spring/ldap/controller/WelcomePageController.java
@@ -0,0 +1,23 @@
+package pl.sda.spring.ldap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomePageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to the home page!";
+    }
+
+    @GetMapping("/developers")
+    public String developers() {
+        return "Welcome to the page for developers only!";
+    }
+
+    @GetMapping("/managers")
+    public String managers() {
+        return "Welcome to the page for managers only!";
+    }
+}
